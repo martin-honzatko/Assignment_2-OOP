1b3fabac4e0ce722c11cf268919cfbb65d5b86bc
Martin Honzatko <43739467+martin-honzatko@users.noreply.github.com>
2023-03-09 18:30:57 -0800

    First Commit

---
+++ Assignment2/Assignment2/Date.swift
@@ -0,0 +1,16 @@
+//
+//  Date.swift
+//  Assignment2
+//
+//  Created by Martin Honzatko on 2023-03-09.
+//
+
+import Foundation
+
+struct Date {
+  
+}
+
+enum DateFormat {
+  case standard, long, two
+}
